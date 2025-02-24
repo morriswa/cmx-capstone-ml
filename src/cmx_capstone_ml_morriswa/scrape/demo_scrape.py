5a9f6f46ea4d0b402a84cb08daaa5fed559c5f09
makennabrynn <makennaloewenherz@gmail.com>
2025-02-23 18:43:46 -0600

    finished scrape algorithm

--- src/cmx_capstone_ml_morriswa/scrape/demo_scrape.py
+++ src/cmx_capstone_ml_morriswa/scrape/demo_scrape.py
@@ -6,7 +6,7 @@ from bs4 import BeautifulSoup
 import pandas as pd
 
 # main function that takes parameter of site to be scrapped
-def scrape_admissions(site_url, data):
+def scrape_function(site_url, dictionary):
 
     # use requests library to get HTML contents of the site
     html_object = requests.get(site_url)
@@ -29,21 +29,18 @@ def scrape_admissions(site_url, data):
     # site titles
     site_titles = soup_object.find_all('title') # saves all title tags to a list (the name that appears on a browser tab)
     cleaned_titles = [title.get_text().strip() for title in site_titles]
+    first_title = cleaned_titles[0]
+    first_part = first_title.split(" | ")[0]
     
     # site department
     site_department = soup_object.find_all(class_="site-title-group__site-title") # saves all instances of the class site-title-group__site-title, which is the blue text at the top left of every ku site
     cleaned_department = [dep.get_text().strip() for dep in site_department]
 
-    data['Department'].append(cleaned_department[0])
-    data['Page Title'].append(cleaned_titles[0])
-    data['h1 Title'].append(cleaned_h1[0])
-    data['Text'].append(cleaned_text)
-    data['URL'].append(site_url)
-
-    # create a pandas dataframe to store information
-    example_dataframe = pd.DataFrame(data)
-    print(example_dataframe)
-
+    dictionary['Department'].append(cleaned_department[0])
+    dictionary['Page Title'].append(first_part)
+    dictionary['h1 Title'].append(cleaned_h1[0])
+    dictionary['Text'].append(cleaned_text)
+    dictionary['URL'].append(site_url)
 
 # save all collected data in a dictionary, which will be used to make the dataframe
 data_dictionary = {'Department': [],
@@ -53,5 +50,11 @@ data_dictionary = {'Department': [],
         'URL': []
         }
 
-# example usage on admissions.ku.edu
-scrape_admissions("https://admissions.ku.edu/afford", data_dictionary)
+# example of a list of sites to scrape
+example_list = ["https://admissions.ku.edu", "https://admissions.ku.edu/afford", "https://admissions.ku.edu/success"]
+for site in example_list: 
+    scrape_function(site, data_dictionary)
+
+# create a pandas dataframe to store information
+example_dataframe = pd.DataFrame(data_dictionary)
+print(example_dataframe)
