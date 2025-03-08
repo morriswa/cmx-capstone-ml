ed1bb858aff0c9d43bfafadb364bca1f8e93cda4
makennabrynn <makennaloewenherz@gmail.com>
2025-03-07 22:37:54 -0600

    fixed scrape algorithm

--- src/cmx_capstone_ml_morriswa/scrape/__init__.py
+++ src/cmx_capstone_ml_morriswa/scrape/__init__.py
@@ -4,5 +4,5 @@ from .web import scrape_urls
 
 
 def scrape(output_file_path):
-    all_urls = get_all_sites()
-    scrape_urls(all_urls, output_file_path)
+    all_urls = get_all_sites() # return a list of urls
+    scrape_urls(all_urls, output_file_path) # create a csv of scraped info  # output_file_path will be the name is the csv
--- src/cmx_capstone_ml_morriswa/scrape/web.py
+++ src/cmx_capstone_ml_morriswa/scrape/web.py
@@ -5,9 +5,22 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 
+def scrape_urls(all_urls, csv_filename) -> None:
+    data_dictionary = {'Department': [],
+        'Page Title': [], # I added this because H1s are not always descriptive
+        'h1 Title': [],
+        'Text': [],
+        'URL': []
+        }
+    for site in all_urls:
+        scrape_function(site, data_dictionary)
 
-def scrape_urls(all_urls, output_file_path) -> None:
-    pass
+    # create a pandas dataframe to store information
+    dataframe = pd.DataFrame(data_dictionary)
+    print(dataframe)
+
+    dataframe.to_csv(csv_filename)
+    
 
 # main function that takes parameter of site to be scrapped
 def scrape_function(site_url, dictionary):
@@ -28,73 +41,66 @@ def scrape_function(site_url, dictionary):
 
     # h1s
     headings = soup_object.find_all('h1') # saves all h1s to a list
+
+    # handle cases where site has no h1
     if headings == []:
         cleaned_h1 = ["N/A"]
     else:
         cleaned_h1 = [heading.get_text().strip() for heading in headings]
-    # print(cleaned_h1)
 
     # site titles
     site_titles = soup_object.find_all('title') # saves all title tags to a list (the name that appears on a browser tab)
+    
+    # handle cases where site has no title tags
     if site_titles == []:
-        first_part = "N/A"
+        first_title_cleaned = "N/A"
     else:
         cleaned_titles = [title.get_text().strip() for title in site_titles]
         first_title = cleaned_titles[0]
-        first_part = first_title.split(" | ")[0]
-    # print(first_part)
+        first_title_cleaned = first_title.split(" | ")[0]
 
     # site department
     site_department = soup_object.find_all(class_="site-title-group__site-title") # saves all instances of the class site-title-group__site-title, which is the blue text at the top left of every ku site
+    
+    # handle cases where site has no site-title-group__site-title tag
     if site_department == []:
-        cleaned_department = ["N/A"]
+        department_cleaned = ["N/A"]
     else:
-        cleaned_department = [dep.get_text().strip() for dep in site_department]
-    # print(cleaned_department)
+        department_cleaned = [dep.get_text().strip() for dep in site_department][0] # return first element in cleaned departments list
 
-    dictionary['Department'].append(cleaned_department[0])
-    dictionary['Page Title'].append(first_part)
+    # add scraped data into a dictionary
+    dictionary['Department'].append(department_cleaned)
+    dictionary['Page Title'].append(first_title_cleaned)
     dictionary['h1 Title'].append(cleaned_h1[0])
     dictionary['Text'].append(cleaned_text)
     dictionary['URL'].append(site_url)
 
-# # save all collected data in a dictionary, which will be used to make the dataframe
-# data_dictionary = {'Department': [],
-#         'Page Title': [], # I added this because H1s are not always descriptive
-#         'h1 Title': [],
-#         'Text': [],
-#         'URL': []
-#         }
 
-# # example of a list of sites to scrape
-# example_list = ["https://accessibility.ku.edu",
-# "https://affordability.ku.edu/costs",
-# "https://canvas.ku.edu",
-# "https://cms.ku.edu",
-# "https://directory.ku.edu/",
-# "https://employment.ku.edu/",
-# "https://financialaid.ku.edu/consumer-information",
-# "https://humanresources.ku.edu/",
-# "https://iss.ku.edu/",
-# "https://ku.edu",
-# "https://kupolice.ku.edu/",
-# "https://lib.ku.edu/",
-# "https://my.ku.edu",
-# "https://my.ku.edu/JayhawkGpsRedirect",
-# "https://news.ku.edu/",
-# "https://opsmaps.ku.edu/",
-# "https://otp.ku.edu/",
-# "https://policy.ku.edu/provost/privacy-policy",
-# "https://publicaffairs.ku.edu/freedom-of-expression",
-# "https://registrar.ku.edu/",
-# "https://registrar.ku.edu/ku-academic-calendar",
-# "https://registrar.ku.edu/transcripts",
-# "https://sa.ku.edu",
-# "https://technology.ku.edu/"]
-#
-# for site in example_list:
-#     scrape_function(site, data_dictionary)
-#
-# # create a pandas dataframe to store information
-# example_dataframe = pd.DataFrame(data_dictionary)
-# print(example_dataframe)
+
+#### testing ####
+# example of a list of sites to scrape for testing 
+example_list = ["https://accessibility.ku.edu",
+"https://affordability.ku.edu/costs",
+"https://canvas.ku.edu",
+"https://cms.ku.edu",
+"https://directory.ku.edu/",
+"https://employment.ku.edu/",
+"https://financialaid.ku.edu/consumer-information",
+"https://humanresources.ku.edu/",
+"https://iss.ku.edu/",
+"https://ku.edu",
+"https://kupolice.ku.edu/",
+"https://lib.ku.edu/",
+"https://my.ku.edu",
+"https://my.ku.edu/JayhawkGpsRedirect",
+"https://news.ku.edu/",
+"https://opsmaps.ku.edu/",
+"https://otp.ku.edu/",
+"https://policy.ku.edu/provost/privacy-policy",
+"https://publicaffairs.ku.edu/freedom-of-expression",
+"https://registrar.ku.edu/",
+"https://registrar.ku.edu/ku-academic-calendar",
+"https://registrar.ku.edu/transcripts",
+"https://sa.ku.edu",
+"https://technology.ku.edu/"]
+scrape_urls(example_list, 'ku_csv')
