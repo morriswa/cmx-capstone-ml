0f327f85fd3eea2be4655711aeeeda9ec4f7ade2
Timothy Holmes <holmestimo11@gmail.com>
2025-04-14 11:45:21 -0500

    added new site_map function name

--- src/scrape/__init__.py
+++ src/scrape/__init__.py
@@ -1,8 +1,8 @@
 
-from .site_map import get_all_sites
+from .site_map import get_all_ku_sites_with_depth_priority
 from .web import scrape_urls
 
 
 def scrape(output_file_path):
-    all_urls = get_all_sites("https://ku.edu/")
+    all_urls = get_all_ku_sites_with_depth_priority("https://ku.edu/")
     scrape_urls(all_urls, output_file_path)
